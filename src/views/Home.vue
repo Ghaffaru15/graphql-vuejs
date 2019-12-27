cdefc222a9dd3c7abbd30fb93fb990f59d333d18
Ghaffaru <mudashiruagm@gmail.com>
2019-12-27 10:14:01 +0000

    vuejs apollo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ul>
+      <li v-for="user in users" :key="user.name">
+        {{ user.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import gql from 'graphql-tag'
 
 export default {
   name: 'home',
+  data() {
+    return {
+      users:[],
+      user: null
+    }
+  },
   components: {
-    HelloWorld
+  
+  },
+  apollo: {
+    users: gql`query{
+      users {
+        name
+        email
+      }
+    }`
   }
 }
 </script>
